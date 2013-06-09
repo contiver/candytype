24b5b5ed37bf938910b4ccc1106ae898516f1f7e
cristian <cristianontivero@gmail.com>
2013-06-09 17:45:25 -0300

    linea innecesaria borrada

--- src/backend/cell/CheckCell.java
+++ src/backend/cell/CheckCell.java
@@ -9,13 +9,6 @@ import backend.move.Direction;
 
 import java.util.Map;
 
-/**
- * Created with IntelliJ IDEA.
- * User: daniel
- * Date: 01/06/13
- * Time: 14:10
- * To change this template use File | Settings | File Templates.
- */
 public class CheckCell extends Cell {
     private Map<FoodType, FoodTypeState> foodMap;
 
@@ -25,7 +18,6 @@ public class CheckCell extends Cell {
     }
 
     public boolean fallUpperContent(){
-        Cell upperCell = this.getAround()[Direction.UP.ordinal()];
         if (this.getContent().isCheckable()){
             foodMap.get(((Food)(this.getContent())).getFoodType()).reachedCheckCell();
             this.setContent(new Nothing());
